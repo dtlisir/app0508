35cdddfd794996365ab16dc77ed601926fa2ec64
dtlisir <dtlisir@163.com>
2019-05-09 18:59:26 +0800

    version 1.1

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+        'NAME': "%s_o" % APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'lisir',                            # 你的数据库user
+        'PASSWORD': '800213',                        # 你的数据库password
+        'HOST': '192.168.24.166',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
